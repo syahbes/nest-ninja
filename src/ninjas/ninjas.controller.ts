6b6029b017105098d9c6edcd434b48215b43fad6
Shlomi <shlomi@chimu.io>
2023-08-07 21:15:25 +0300

    num4 - 5min

--- src/ninjas/ninjas.controller.ts
+++ src/ninjas/ninjas.controller.ts
@@ -1,29 +1,52 @@
-import { Controller, Delete, Get, Post, Put } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+  Query
+} from "@nestjs/common";
+import { CreateNinjaDto } from "./dto/create-ninja.dto";
+import { NinjasService } from "./ninjas.service";
 
 @Controller('ninjas')
 export class NinjasController {
+  constructor(private readonly ninjasService: NinjasService) { }
+
+  //Get /ninjas?type=fast
   // GET /ninjas --> []
   @Get()
-  getNinjas() {
-    return [];
+  getNinjas(@Query('weapon') weapon: 'stars' | 'swords' | 'nunchucks') {
+    // const service = new NinjasService();
+    return this.ninjasService.getNinjas(weapon);
   }
 
   // GET /ninjas/:id --> {...}
   @Get(':id')
-  getOneNinja() {
-    return {};
+  getOneNinja(@Param('id') id: string) {
+    return { id };
   }
 
   // POST /ninjas --> {...}
   @Post()
-  createNinja() {
-    return {};
+  createNinja(@Body() createNinjaDto: CreateNinjaDto) {
+    return {
+      ...createNinjaDto
+      // name: createNinjaDto.name
+    };
   }
 
   // PUT /ninjas/:id --> {...}
   @Put('id')
-  updateNinja() {
-    return {};
+  updateNinja(@Param('id') id: string, @Body() createNinjaDto: CreateNinjaDto) {
+    return {
+      id,
+      // ...createNinjaDto
+      name: createNinjaDto.name
+    };
+
   }
 
   // DELETE /ninjas/:id
--- src/ninjas/ninjas.service.ts
+++ src/ninjas/ninjas.service.ts
@@ -1,4 +1,24 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class NinjasService {}
+export class NinjasService {
+  private ninjas = [
+    { id: 0, name: 'Red Ninja', weapon: 'stars' },
+    { id: 1, name: 'Blue Ninja', weapon: 'swords' },
+    { id: 2, name: 'Yellow Ninja', weapon: 'nunchucks' },
+  ];
+  getNinjas(weapon?: 'stars' | 'swords' | 'nunchucks') {
+    if (weapon) {
+      return this.ninjas.filter((ninja) => ninja.weapon === weapon);
+    }
+    return this.ninjas;
+  }
+
+  getNinja(id: number) {
+    const ninja = this.ninjas.find((ninja) => ninja.id === id);
+    if (!ninja) {
+      throw new Error('Ninja not found');
+    }
+    return ninja;
+  }
+}
