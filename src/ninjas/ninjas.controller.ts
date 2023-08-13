ddf3b109fdb939fb2fca957b25e483cd79d3863f
Shlomi <shlomi@chimu.io>
2023-08-13 09:07:02 +0300

    end of leasson 7 need to practice and learn more about guards

--- src/ninjas/ninjas.controller.ts
+++ src/ninjas/ninjas.controller.ts
@@ -9,11 +9,13 @@ import {
   Post,
   Put,
   Query,
+  UseGuards,
   ValidationPipe,
 } from '@nestjs/common';
 import { CreateNinjaDto } from './dto/create-ninja.dto';
 import { NinjasService } from './ninjas.service';
 import { UpdateNinjaDto } from './dto/update-ninja.dto';
+import { BeltGuard } from '../belt/belt.guard';
 
 @Controller('ninjas')
 export class NinjasController {
@@ -39,6 +41,7 @@ export class NinjasController {
 
   // POST /ninjas --> {...}
   @Post()
+  @UseGuards(BeltGuard)
   createNinja(@Body(new ValidationPipe()) createNinjaDto: CreateNinjaDto) {
     return this.ninjasService.createNinja(createNinjaDto);
   }
